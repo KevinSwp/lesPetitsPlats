39b398878712703d90bf66431e73e6a2177c4b02
kevin swp <thranva@gmail.com>
2023-03-28 09:41:31 +0200

    update view index

--- scripts/controllers/index_action.js
+++ scripts/controllers/index_action.js
@@ -8,8 +8,18 @@ import IndexView from "../views/index_view.js";
 /**
  * Select element from DOM
  */
+const filter = document.getElementById("dropdown");
 const searchBarSection = document.getElementById("searchBarSection");
 const recipeSection = document.querySelector(".recipes-list");
+
+/**
+ * Display search bar
+ */
+const displayDropdown = () => {
+    const objectIndexView = new IndexView();
+    const dropdown = objectIndexView.getDropdownFilter();
+    filter.appendChild(dropdown);
+};
 
 /**
  * Display search bar
@@ -23,17 +33,22 @@ const displaySearchBar = () => {
 /**
  * Display all recipes
  */
-const displayRecipes = (recipes) => {
+const displayRecipes = (recipes, searchText) => {
     recipes.forEach((recipeData) => {
         // Build recipe object from data
         const recipe = new RecipeFactory(recipeData, RECIPE_TYPES.JSON_V1);
 
-        // Build recipe card from object
-        const recipeCardView = new IndexView(recipe);
-        const recipeCard = recipeCardView.getRecipeCardIndex();
+        // Check if the recipe name or description includes the search text
+        const recipeTitle = recipe.name.toLowerCase();
+        const recipeDescription = recipe.description.toLowerCase();
+        const includesSearchText = recipeTitle.includes(searchText) || recipeDescription.includes(searchText);
 
-        // Add recipe card as child
-        recipeSection.appendChild(recipeCard);
+        // If the recipe matches the search criteria, build and display the recipe card
+        if (includesSearchText) {
+            const recipeCardView = new IndexView(recipe);
+            const recipeCard = recipeCardView.getRecipeCardIndex();
+            recipeSection.appendChild(recipeCard);
+        }
     });
 };
 
@@ -47,27 +62,27 @@ const init = () => {
         .then((data) => {
             const { recipes } = data;
 
+            // Display dropdown
+            displayDropdown();
+
             // Display search bar
             displaySearchBar();
 
             // Display all recipes
             displayRecipes(recipes);
 
+
+
             // Filter recipes from search bar
             const searchInput = document.querySelector("#searchBarSection input");
             searchInput.addEventListener("input", () => {
                 const searchText = searchInput.value.toLowerCase().trim();
-                const filteredRecipes = recipes.filter((recipeData) => {
-                    const recipe = new RecipeFactory(recipeData, RECIPE_TYPES.JSON_V1);
-                    const recipeTitle = recipe.name.toLowerCase();
-                    return recipeTitle.includes(searchText);
-                });
 
                 // Empty recipes section
                 recipeSection.innerHTML = "";
 
                 // Display filtered recipes
-                displayRecipes(filteredRecipes);
+                displayRecipes(recipes, searchText);
             });
         })
         .catch((error) => {
--- scripts/views/index_view.js
+++ scripts/views/index_view.js
@@ -4,15 +4,38 @@ export default class IndexView {
     }
 
     //Function content home page
+    getDropdownFilter = () => {
+        // Create article element in the DOM
+        const div = document.createElement('div');
+        // Fill the DOM
+        div.innerHTML = `
+        <div class="btn-group bouton-group">
+            <button type="button" class="btn btn-primary btn-lg dropdown-toggle">
+                Ingredients
+            </button>
+            <button type="button" class="btn btn-success btn-lg dropdown-toggle">
+                appareils
+            </button>
+            <button type="button" class="btn btn-danger btn-lg dropdown-toggle">
+                Ustensiles
+            </button>
+        </div>
+        `;
+
+        return div;
+    }
+
+    //Function content home page
     getSearchBar = () => {
         // Create article element in the DOM
         const div = document.createElement('div');
         // Fill the DOM
         div.innerHTML = `
-            <form action="#" method="get">
-                <label for="search">Recherche:</label>
-                <input type="text" id="search" name="q">
-                <button type="submit">Rechercher</button>
+            <form class="formSearchBar">
+                <input class="searchInput" type="text" id="search" placeholder="Rechercher une recette">
+                <svg class="magnifying_glass" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+                </svg>
             </form>
         `;
 
@@ -23,11 +46,129 @@ export default class IndexView {
     getRecipeCardIndex = () => {
         // Create article element in the DOM
         const div = document.createElement('div');
+        div.classList.add("figureRecipe");
+        /*// Fill the DOM
+        if (this._recipe.ingredients[0].unit === undefined) {
+            div.innerHTML = `
+                <figure>
+                    <img class="imageRecipe" src="../assets/images/recette.jpg">
+                    <figcaption>
+                        <h1 class="titleRecipe">${this._recipe.name}</h1>
+                        <div class="ingredient">
+                            <p>${this._recipe.ingredients[0].ingredient} :</p>
+                            <p>${this._recipe.ingredients[0].quantity}</p>
+                        </div>
+                        <div class="ingredient">
+                            <p>${this._recipe.ingredients[1].ingredient}</p>
+                            <p>${this._recipe.ingredients[1].quantity}</p>
+                        </div>
+                        <div class="ingredient">
+                            <p>${this._recipe.ingredients[2].ingredient} :</p>
+                            <p>${this._recipe.ingredients[2].quantity}</p>
+                        </div>
+                        <div class="ingredient">
+                            <p>${this._recipe.ingredients[3].ingredient}</p>
+                            <p>${this._recipe.ingredients[3].quantity}</p>
+                        </div>
+                        <div class="ingredient">
+                            <p>${this._recipe.ingredients[4].ingredient} :</p>
+                            <p>${this._recipe.ingredients[4].quantity}</p>
+                        </div>
+                        <p class="description">${this._recipe.description}</p>
+                    </figcaption>
+                </figure>  
+            `;
+        }
+        else {
+            div.innerHTML = `
+                <figure>
+                    <img class="imageRecipe" src="../assets/images/recette.jpg">
+                    <figcaption>
+                        <h1 class="titleRecipe">${this._recipe.name}</h1>
+                        <div class="ingredient">
+                            <p>${this._recipe.ingredients[0].ingredient} :</p>
+                            <p>${this._recipe.ingredients[0].quantity}</p>
+                            <p>${this._recipe.ingredients[0].unit}</p>
+                        </div>
+                        <div class="ingredient">
+                            <p>${this._recipe.ingredients[1].ingredient}</p>
+                            <p>${this._recipe.ingredients[1].quantity}</p>
+                            <p>${this._recipe.ingredients[1].unit}</p>
+                        </div>
+                        <div class="ingredient">
+                            <p>${this._recipe.ingredients[2].ingredient} :</p>
+                            <p>${this._recipe.ingredients[2].quantity}</p>
+                            <p>${this._recipe.ingredients[2].unit}</p>
+                        </div>
+                        <div class="ingredient">
+                            <p>${this._recipe.ingredients[3].ingredient}</p>
+                            <p>${this._recipe.ingredients[3].quantity}</p>
+                            <p>${this._recipe.ingredients[3].unit}</p>
+                        </div>
+                        <div class="ingredient">
+                            <p>${this._recipe.ingredients[4].ingredient} :</p>
+                            <p>${this._recipe.ingredients[4].quantity}</p>
+                            <p>${this._recipe.ingredients[4].unit}</p>
+                        </div>
+                        <p class="description">${this._recipe.description}</p>
+                    </figcaption>
+                </figure>  
+            `;
+        }*/
         // Fill the DOM
-        div.innerHTML = `
-            <h1 class="titleRecipe">${this._recipe.name}</h1>
-        `;
-
+        if (this._recipe.ingredients[0].unit === undefined) {
+            div.innerHTML = `
+                <figure>
+                    <img class="imageRecipe" src="../assets/images/recette.jpg">
+                    <figcaption>
+                        <div class="title_time">
+                            <h1 class="titleRecipe">${this._recipe.name}</h1>
+                            <p>${this._recipe.time} min</p>
+                        </div>
+                        <div class="ingredients_description">
+                            <div>
+                                <div class="ingredient">
+                                    <p>${this._recipe.ingredients[0].ingredient}:</p>
+                                    <p>${this._recipe.ingredients[0].quantity}</p>
+                                </div>
+                                <div class="ingredient">
+                                    <p>${this._recipe.ingredients[1].ingredient}:</p>
+                                </div>
+                            </div>
+                            <p class="description">${this._recipe.description}</p>
+                        </div>
+                    </figcaption>
+                </figure>  
+            `;
+        }
+        else {
+            div.innerHTML = `
+                <figure>
+                    <img class="imageRecipe" src="../assets/images/recette.jpg">
+                    <figcaption>
+                        <div class="title_time">
+                            <h1 class="titleRecipe">${this._recipe.name}</h1>
+                            <p>${this._recipe.time} min</p>
+                        </div>
+                        <div class="ingredients_description">
+                            <div>
+                                <div class="ingredient">
+                                    <p>${this._recipe.ingredients[0].ingredient}:</p>
+                                    <p>${this._recipe.ingredients[0].quantity}</p>
+                                    <p>${this._recipe.ingredients[0].unit}</p>
+                                </div>
+                                <div class="ingredient">
+                                    <p>${this._recipe.ingredients[1].ingredient}:</p>
+                                    <p>${this._recipe.ingredients[1].quantity}</p>
+                                </div>
+                            </div>
+                            <p class="description">${this._recipe.description}</p>
+                        </div>
+                    </figcaption>
+                </figure>  
+            `;
+        }
+                    
         return div;
     }
 
