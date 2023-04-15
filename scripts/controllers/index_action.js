f9e22bdc00cfdeb8ffcc07248477f2a87f249787
kevin swp <thranva@gmail.com>
2023-04-16 01:33:25 +0200

    tag filter update

--- scripts/controllers/index_action.js
+++ scripts/controllers/index_action.js
@@ -1,87 +1,126 @@
 /**
  * Import
- */
+*/
 import RecipeFactory from "../factories/RecipeFactory.js";
 import { RECIPE_TYPES } from "../factories/RecipeFactory.js";
 import IndexView from "../views/index_view.js";
 import { showFilterDropdown } from "../utils/filters.js";
 
-/**
- * Select element from DOM
- */
-const filter = document.getElementById("dropdownFilter");
-const searchBarSection = document.getElementById("searchBarSection");
-const recipeSection = document.querySelector(".recipes-list");
+document.addEventListener("DOMContentLoaded", () => {
+    /**
+     * Select elements from DOM
+     */
+    const filter = document.getElementById("dropdownFilter");
+    const searchBarSection = document.getElementById("searchBarSection");
+    const recipeSection = document.querySelector(".recipes-list");
 
-/**
- * Reset la liste des recettes
- */
-const resetDisplayRecipes = () => {
-    recipeSection.innerHTML = "";
-}
+    const getAllRecipes = async () => {
+        try {
+            const response = await fetch("data/recipes.json");
+            const data = await response.json();
+            const { recipes } = data;
+            const recipeObjects = recipes.map((recipeData) => new RecipeFactory(recipeData, RECIPE_TYPES.JSON_V1));
 
-/**
- * Display all recipes
- */
-const displayAllRecipes = (recipes) => {
-    recipes.forEach((recipeData) => {
-        // Build recipe object from data
-        const recipe = new RecipeFactory(recipeData, RECIPE_TYPES.JSON_V1);
-        // Build and display the recipe card
-        const recipeCardView = new IndexView(recipe);
-        const recipeCard = recipeCardView.getRecipeCardIndex();
-        recipeSection.appendChild(recipeCard);
-    });
-};
+            return recipeObjects;
+        }
+        catch (error) {
+            console.error(`Error fetching data: ${error}`);
+            throw error;
+        }
+    };
 
-/**
- * Display search bar
- */
-const displaySearchBar = () => {
-    const searchBarView = new IndexView();
-    const searchBar = searchBarView.getSearchBar();
-    searchBarSection.appendChild(searchBar);
-};
+    /**
+     * Display search bar
+     */
+    const displaySearchBar = () => {
+        const searchBarView = new IndexView();
+        const searchBar = searchBarView.getSearchBar();
+        searchBarSection.appendChild(searchBar);
 
-/**
- * Display recipes that match the search criteria
- */
-const displaySearchRecipes = (recipes, searchText) => {
+        // On ajoute le listener sur la search bar
+        searchBar.addEventListener("input", async () => {
+            const allRecipes = await getAllRecipes();
+            const recipes = sortAll(allRecipes);
+            displayAllRecipes(recipes);
+            // displayDropdownIngredient(recipes);
+        });
+    };
 
-    recipes.forEach((recipeData) => {
-        // Build recipe object from data
-        const recipe = new RecipeFactory(recipeData, RECIPE_TYPES.JSON_V1);
+    const sortByKeywords = (recipes) => {
+        // Filter recipes from search bar
+        const searchInput = document.querySelector("#searchBarSection input");
+        const searchText = searchInput.value.toLowerCase().trim();
 
-        // Check if the recipe name or description includes the search text
-        const recipeTitle = recipe.name.toLowerCase();
-        const recipeDescription = recipe.description.toLowerCase();
-        const includesSearchText = recipeTitle.includes(searchText) || recipeDescription.includes(searchText);
+        // Empty recipes section
+        recipeSection.innerHTML = "";
 
-        // If the recipe matches the search criteria, build and display the recipe card
-        if (includesSearchText) {
+        // Display filtered recipes
+        const recipesOK = [];
+        recipes.forEach((recipe) => {
+            // Check if the recipe name or description includes the search text
+            const recipeTitle = recipe.name.toLowerCase();
+            const recipeDescription = recipe.description.toLowerCase();
+            const includesSearchText = recipeTitle.includes(searchText) || recipeDescription.includes(searchText);
+
+            // If the recipe matches the search criteria, build and display the recipe card
+            if (includesSearchText) {
+                const recipeCardView = new IndexView(recipe);
+                const recipeCard = recipeCardView.getRecipeCardIndex();
+                recipeSection.appendChild(recipeCard);
+                recipesOK.push(recipe);
+            }
+        });
+
+        return recipesOK;
+    };
+
+    const filterByDropdownSelection = (recipes) => {
+        const selectedTags = Array.from(document.querySelectorAll("#tagsFilter button"))
+            .map(tagButton => tagButton.textContent.toLowerCase());
+    
+        if (selectedTags.length === 0) {
+            return recipes;
+        }
+    
+        return recipes.filter(recipe => {
+            const recipeIngredients = recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase());
+            return selectedTags.every(tag => recipeIngredients.includes(tag));
+        });
+    };
+
+    const sortAll = async (recipesPromise) => {
+        const recipes = await recipesPromise;
+        const filteredByKeywords = sortByKeywords(recipes);
+        const filteredByDropdownSelection = filterByDropdownSelection(filteredByKeywords);
+        
+        return filteredByDropdownSelection;
+    };
+
+    /**
+     * Reset la liste des recettes
+     */
+    const resetDisplayRecipes = () => {
+        recipeSection.innerHTML = "";
+    }
+
+    /**
+     * Display all recipes
+     */
+    const displayAllRecipes = (recipes) => {
+        resetDisplayRecipes();
+
+        recipes.forEach((recipe) => {
+            // Build and display the recipe card
             const recipeCardView = new IndexView(recipe);
             const recipeCard = recipeCardView.getRecipeCardIndex();
             recipeSection.appendChild(recipeCard);
-        }
-    });
-};
-
-/**
- * Delete tag on click
- */
-const deleteTags = () => {
-    document.querySelectorAll("#tagsFilter button").forEach((button) => {
-      button.addEventListener("click", (event) => {
-        button.parentNode.removeChild(button);
-        // document.querySelector(".dropdown_content").appendChild(event.target);
-      });
-    });
-  };
+        });
+    };
 
 /**
  * Display dropdown
 */
-const displayDropdown = (recipes) => {
+const displayDropdownIngredient = (recipes) => {
     const objectIndexView = new IndexView();
     const dropdown = objectIndexView.getFilter();
     filter.appendChild(dropdown);
@@ -89,23 +128,61 @@ const displayDropdown = (recipes) => {
     const searchInput = document.getElementById("searchFilter");
     const dropdownContent = document.querySelector(".dropdown_content");
     
-    searchInput.addEventListener("input", () => {
-        const searchKeyword = searchInput.value.toLowerCase();
-        dropdownContent.innerHTML = "";
-        recipes.forEach(recipeData => {
-            const recipe = new RecipeFactory(recipeData, RECIPE_TYPES.JSON_V1);
-            recipe.ingredients.forEach(ingredient => {
-                if (ingredient.ingredient.toLowerCase().includes(searchKeyword)) {
-                    dropdownContent.innerHTML += `<button>${ingredient.ingredient}</button>`;
-                } else {
-                    dropdownContent.innerHTML += `<button>${ingredient.ingredient}</button>`;
-                }
+    // On affiche une première fois tous le monde qui match
+    dropdownContent.innerHTML = "";
+    const uniqueIngredients = new Set();
+    recipes.forEach(recipe => {
+        recipe.ingredients.forEach(ingredient => {
+            uniqueIngredients.add(ingredient.ingredient.toLowerCase());
+        });
+    });
+
+    uniqueIngredients.forEach(ingredient => {
+        dropdownContent.innerHTML += `<button>${ingredient}</button>`;
+    });
+    
+    const buttons = document.querySelectorAll(".dropdown_content button");
+    buttons.forEach(button => {
+        button.addEventListener("click", async (event) => {
+            // On l'ajoute dans tagsFilter
+            const newButton = document.createElement('button')
+            newButton.textContent = event.target.textContent;
+            document.querySelector("#tagsFilter").appendChild(newButton);
+            
+            // On relance toutes les fonctons de trie
+            const allRecipes = await getAllRecipes();
+            const recipes = await sortAll(allRecipes);
+            displayAllRecipes(recipes);
+            displayDropdownIngredient(recipes);  
+        
+            newButton.addEventListener('click', async () => {
+                // On supprime le bouton de tagsFilter
+                newButton.remove();
+                const allRecipes = await getAllRecipes();
+                const recipes = await sortAll(allRecipes);
+                displayAllRecipes(recipes);
+                displayDropdownIngredient(recipes);  
             });
         });
+    });
+
+    // Display matching ingredient from the search bar
+    searchInput.addEventListener("input", () => {
+        
+        const searchKeyword = searchInput.value.toLowerCase();
+        dropdownContent.innerHTML = "";
+        
+        const filteredIngredients = Array.from(uniqueIngredients).filter(ingredient => ingredient.includes(searchKeyword));
+    
+        filteredIngredients.forEach(ingredient => {
+            dropdownContent.innerHTML += `<button>${ingredient}</button>`;
+        });
+    
         const buttons = document.querySelectorAll(".dropdown_content button");
         buttons.forEach(button => {
             button.addEventListener("click", (event) => {
                 document.querySelector("#tagsFilter").appendChild(event.target);
+                // Faire pareil que au-dessus
             });
         });
     });
@@ -118,45 +195,26 @@ const displayDropdown = (recipes) => {
 
 };
 
-const sortByKeywords = (recipes) => {
-    // Filter recipes from search bar
-    const searchInput = document.querySelector("#searchBarSection input");
-    searchInput.addEventListener("input", () => {
-        const searchText = searchInput.value.toLowerCase().trim();
 
-        // Empty recipes section
-        resetDisplayRecipes();
+    /**
+     * Init
+     */
+    const init = async () => {
+        // Display search bar
+        displaySearchBar();
+    
+        // Get all recipes
+        const allRecipesPromise = getAllRecipes();
+    
+        // Sort all recipes by keyword, tags 
+        const recipes = await sortAll(allRecipesPromise);
+    
+        // Reset and display recipes cards
+        displayAllRecipes(recipes);
+    
+        // Display dropdown
+        displayDropdownIngredient(recipes);
+    };
 
-        // Display filtered recipes
-        displaySearchRecipes(recipes, searchText);
-    });
-}
-
-/**
- * Init
- */
-const init = () => {
-    // Load recipes data
-    fetch("data/recipes.json")
-        .then((response) => response.json())
-        .then((data) => {
-            const { recipes } = data;
-
-            // Delete tags
-            deleteTags();
-            // Display dropdown
-            displayDropdown(recipes);
-            // Display search bar
-            displaySearchBar();
-            // Display all recipes
-            displayAllRecipes(recipes);
-            sortByKeywords(recipes);
-
-        })
-        .catch((error) => {
-            console.error(`Error fetching data: ${error}`);
-            document.querySelector("#main").innerHTML = "Impossible d'afficher les recettes.";
-        });
-};
-
-init();
+    init();
+});
--- scripts/utils/filters.js
+++ scripts/utils/filters.js
@@ -5,31 +5,20 @@ export const showFilterDropdown = () => {
     const selected = document.getElementById("selected");
     const svg = document.querySelector(".svgIcon svg");
     const option = document.querySelector(".dropdown_content");
+    const formSearchBarFilter = document.querySelector(".formSearchBarFilter");
 
     if (option.style.display != "block") {
         // Open dropdown
         option.style.display = "block";
+        formSearchBarFilter.style.display = "block"; // Ajout de cette ligne
         selected.classList.add("selectedActive");
         svg.classList.add("svgEnable");
         svg.classList.remove("svgDisable");
-    }
-    
-    else {
+    } else {
         option.style.display = "none";
+        formSearchBarFilter.style.display = "none"; // Ajout de cette ligne
         selected.classList.remove("selectedActive");
         svg.classList.add("svgDisable");
         svg.classList.remove("svgEnable");
     }
-}
-
-/**
- * Hide filter
- */
-const hideFilterDropdown = () => {
-    const selected = document.getElementById("selected");
-    const svg = document.querySelector(".svgIcon svg");
-    const option = document.querySelector(".dropdown_content");
-    option.style.display = "none";
-    selected.classList.remove("selectedActive");
-    svg.classList.add("svgDisable");
-}
+};
